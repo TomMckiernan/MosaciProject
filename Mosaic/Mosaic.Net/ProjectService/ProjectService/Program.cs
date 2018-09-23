4597d5095ad4048ffd3728602fa51e192c34a092
TomMcKiernan <thomas.mckiernan-2@student.manchester.ac.uk>
2018-09-23 11:56:10 +0100

    0003 Template for project service with test project

---
+++ Mosaic/Mosaic.Net/ProjectService/ProjectService/Program.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using System;
+
+namespace ProjectService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient dbClient = new MongoClient("mongodb://127.0.0.1:27017");
+
+            //Get Database and Collection  
+            IMongoDatabase db = dbClient.GetDatabase("test");
+            var collList = db.ListCollections().ToList();
+            Console.WriteLine("The list of collections are :");
+            foreach (var item in collList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
