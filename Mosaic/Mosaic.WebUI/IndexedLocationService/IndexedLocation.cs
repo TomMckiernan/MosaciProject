11af76dfb2b6d78ffe2a41cbf54d56d528aa2ebb
TomMcKiernan <thomas.mckiernan-2@student.manchester.ac.uk>
2018-10-12 15:52:52 +0100

    0017 Initial commit services moved into same solution as frontend

---
+++ Mosaic/Mosaic.WebUI/IndexedLocationService/IndexedLocation.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndexedLocationService
+{
+    public class IndexedLocation
+    {
+        public string Location { get; set; }
+    }
+}
---
+++ Mosaic/Mosaic.WebUI/IndexedLocationService/IndexedLocationResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndexedLocationService
+{
+    public class IndexedLocationResponse
+    {
+        public string Location { get; set; }
+
+        public string Error { get; set; }
+    }
+}
