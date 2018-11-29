dfddd15098820e51db421b5b5f3f3fc899b97df8
TomMcKiernan <thomas.mckiernan-2@student.manchester.ac.uk>
2018-11-29 09:33:13 +0000

    0010 Testing for new model that image can be copied and deleted

--- Mosaic/Mosaic.WebUI/Mosaic.WebUI/Models/ViewImageModel.cs
+++ Mosaic/Mosaic.WebUI/Mosaic.WebUI/Models/ViewImageModel.cs
@@ -8,28 +8,41 @@ namespace Mosaic.WebUI.Models
 {
     public class ViewImageModel
     {
-        private readonly string ROOT_IMAGE_FOLDER = "~\\images\\temp\\";
+        public string CopyPath { get; set; }
         public string FilePath { get; set; }
         public string Error { get; set; }
 
+        public ViewImageModel(string copyPath = "~\\images\\temp\\")
+        {
+            CopyPath = copyPath;
+        }
+
         public void CopyImage(string fileToCopy)
         {
-            if (File.Exists(fileToCopy))
+            if (Directory.Exists(CopyPath))
             {
-                var newfile = ROOT_IMAGE_FOLDER + Path.GetFileName(fileToCopy);
-                File.Copy(fileToCopy, newfile);
-                FilePath = newfile;
+                if (File.Exists(fileToCopy))
+                {
+                    var newfile = CopyPath + Path.GetFileName(fileToCopy);
+                    File.Copy(fileToCopy, newfile);
+                    FilePath = newfile;
+                }
+                else
+                {
+                    Error = "File to copy does not exist";
+                }
             }
             else
             {
-                Error = "File to copy does not exist";
+                Error = "Image directory does not exist";
             }
+            
         }
 
         // Deletes the current image that has been copied previous
         public void DeleteImage()
         {
-            var di = new DirectoryInfo(ROOT_IMAGE_FOLDER);
+            var di = new DirectoryInfo(CopyPath);
             foreach (FileInfo file in di.GetFiles())
             {
                 file.Delete();
--- Mosaic/Mosaic.WebUI/Mosaic.WebUITests/Models/ViewImageModelTests.cs
+++ Mosaic/Mosaic.WebUI/Mosaic.WebUITests/Models/ViewImageModelTests.cs
@@ -2,6 +2,7 @@
 using Mosaic.WebUI.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Mosaic.WebUITests.Models
@@ -9,28 +10,31 @@ namespace Mosaic.WebUITests.Models
     [TestClass]
     public class ViewImageModelTests
     {
+        public string copyLocation = "C:\\Users\\Tom_m\\OneDrive\\Documents\\MosaicProject\\Mosaic\\Mosaic.WebUI\\Mosaic.WebUI\\wwwroot\\images\\temp\\";
+
         [TestMethod]
-        public void IfFileToCopyDoesNotExistReturnError()
+        public void IfCopyDirectoryDoesNotExistReturnsError()
         {
-            var model = new ViewImageModel();
+            var model = new ViewImageModel("~\\InvalidLocation\\");
             model.CopyImage("~\\InvalidPath\\image.png");
             Assert.IsFalse(String.IsNullOrEmpty(model.Error));
         }
 
         [TestMethod]
-        public void IfFileNotCopiedReturnsError()
+        public void IfFileToCopyDoesNotExistReturnError()
         {
-            //var model = new ViewImageModel();
-            //model.CopyImage("~\\images\\temp");
-            //Assert.IsFalse(String.IsNullOrEmpty(model.Error));
+            var model = new ViewImageModel(copyLocation);
+            model.CopyImage("~\\InvalidPath\\image.png");
+            Assert.IsFalse(String.IsNullOrEmpty(model.Error));
         }
 
         [TestMethod]
         public void IfFileCopiedThenFilePathSetToNewFile()
         {
-            var model = new ViewImageModel();
-            model.CopyImage("..\\..\\..\\..\\..\\..\\test\\TestImages\\752.jpg");
-            Assert.AreEqual("",model.FilePath);
+            var filePath = "..\\..\\..\\..\\..\\..\\test\\TestImages\\752.jpg";
+            var model = new ViewImageModel(copyLocation);
+            model.CopyImage(filePath);
+            Assert.AreEqual(copyLocation + Path.GetFileName(filePath), model.FilePath);
 
             // cleanup
             model.DeleteImage();
