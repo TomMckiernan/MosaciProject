0e1017de8c95ee69e4af4dd71568b57373caed4d
TomMcKiernan <thomas.mckiernan-2@student.manchester.ac.uk>
2018-12-06 14:50:30 +0000

    0036 Create model to be used to construct project cards

---
+++ Mosaic/Mosaic.WebUI/Mosaic.WebUI/Models/ProjectCardModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mosaic.WebUI.Models
+{
+    public class ProjectCardModel
+    {
+        public string ProjectId { get; set; }
+        public int TileImageCount { get; set; }
+        public string MosaicLocation { get; set; }
+        public ProjectStructure.Types.State State { get; set; }
+        public string MasterFileName { get; set; }
+    }
+}
