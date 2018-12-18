681f31ba9308d9c29ccb8868d14c492719a60de9
TomMcKiernan <thomas.mckiernan-2@student.manchester.ac.uk>
2018-12-18 18:50:33 +0000

    0037 Tiled image displayed need to refelct current tile size and ajust to display while image

--- Mosaic/Mosaic.WebUI/Mosaic.WebUI/wwwroot/js/TileImage.js
+++ Mosaic/Mosaic.WebUI/Mosaic.WebUI/wwwroot/js/TileImage.js
@@ -1,18 +1,21 @@
 ﻿can = document.getElementById("canvas");
 var ctxImage = can.getContext("2d");
-
 var im = new Image();
 
 im.onload = function () {
+    var scaleFactor = 540 / im.width;
+    debugger;
     var tileWidth = im.width / 10;
     var tileHeight = im.height / 10;
     debugger;
-    for (var i = 0; i < 20; i++) {
-        for (var j = 0; j < 20; j++) {
+    for (var i = 0; i * 40 < im.width; i++) {
+        for (var j = 0; j * 40 < im.height; j++) {
             var x = i * 40;
             var y = j * 40;
             // image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight
-            ctxImage.drawImage(im, x, y, 40, 40, x + i * 2, y + j * 2, 40, 40);
+            ctxImage.drawImage(im, x, y, 40, 40,              // source tile
+                (x + i * 5) * scaleFactor, (y + j * 5) * scaleFactor, 40 * scaleFactor, 40 * scaleFactor); // destination tile
         }       
     }
+    debugger;
 }
