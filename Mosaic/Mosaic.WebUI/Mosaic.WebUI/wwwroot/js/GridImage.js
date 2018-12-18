a8f1aa4dbaba0cc3ac55db516270357707169bd2
TomMcKiernan <thomas.mckiernan-2@student.manchester.ac.uk>
2018-12-18 19:35:38 +0000

    0037 Canvas script displays image with grid ready to be customised

--- Mosaic/Mosaic.WebUI/Mosaic.WebUI/wwwroot/js/GridImage.js
+++ Mosaic/Mosaic.WebUI/Mosaic.WebUI/wwwroot/js/GridImage.js
@@ -1,30 +1,64 @@
 ﻿can = document.getElementById("canvas");
 var ctxImage = can.getContext("2d");
-
-var data = '<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"> \
-        <defs> \
-            <pattern id="smallGrid" width="8" height="8" patternUnits="userSpaceOnUse"> \
-                <path d="M 8 0 L 0 0 0 8" fill="none" stroke="gray" stroke-width="0.5" /> \
-            </pattern> \
-            <pattern id="grid" width="80" height="80" patternUnits="userSpaceOnUse"> \
-                <rect width="80" height="80" fill="url(#smallGrid)" /> \
-                <path d="M 80 0 L 0 0 0 80" fill="none" stroke="gray" stroke-width="1" /> \
-            </pattern> \
-        </defs> \
-        <rect width="100%" height="100%" fill="url(#smallGrid)" /> \
-    </svg>';
-
-var DOMURL = window.URL || window.webkitURL || window;
-
 var im = new Image();
-var svg = new Blob([data], { type: 'image/svg+xml;charset=utf-8' });
-var url = DOMURL.createObjectURL(svg);
-debugger;
+
+var gridOptions = {
+    minorLines: {
+        separation: 10,
+        color: '#FFFFFF'
+    },
+    majorLines: {
+        separation: 30,
+        color: '#FF0000'
+    }
+};
+
 im.onload = function () {
     var scaleFactor = 540 / im.width;
     debugger;
     ctxImage.drawImage(im, 0, 0, im.width, im.height,              // source tile
         0, 0, im.width * scaleFactor, im.height * scaleFactor); // destination tile
     debugger;
-    DOMURL.revokeObjectURL(url);
+    drawGridLines(gridOptions.minorLines);
+    debugger;
+    //drawGridLines(gridOptions.majorLines);
+}
+
+function drawGridLines(lineOptions) {
+    var scaleFactor = 540 / im.width;
+
+    var iWidth = im.width * scaleFactor;
+    var iHeight = im.height * scaleFactor;
+
+    ctxImage.strokeStyle = lineOptions.color;
+    ctxImage.strokeWidth = 0.1;
+
+    ctxImage.beginPath();
+
+    var iCount = null;
+    var i = null;
+    var x = null;
+    var y = null;
+
+    iCount = Math.floor(iWidth / lineOptions.separation);
+
+    for (i = 1; i <= iCount; i++) {
+        x = (i * lineOptions.separation);
+        ctxImage.moveTo(x, 0);
+        ctxImage.lineTo(x, iHeight);
+        ctxImage.stroke();
+    }
+
+    iCount = Math.floor(iHeight / lineOptions.separation);
+
+    for (i = 1; i <= iCount; i++) {
+        y = (i * lineOptions.separation);
+        ctxImage.moveTo(0, y);
+        ctxImage.lineTo(iWidth, y);
+        ctxImage.stroke();
+    }
+
+    ctxImage.closePath();
+
+    return;
 }
