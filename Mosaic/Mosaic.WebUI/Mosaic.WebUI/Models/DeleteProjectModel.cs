0f762cfcfdff66a6c58112df9dde4e7533dd8972
TomMcKiernan <thomas.mckiernan-2@student.manchester.ac.uk>
2018-12-20 20:12:41 +0000

    0042 Creation of model to delete project

---
+++ Mosaic/Mosaic.WebUI/Mosaic.WebUI/Models/DeleteProjectModel.cs
@@ -0,0 +1,58 @@
+﻿using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mosaic.WebUI.Models
+{
+    public class DeleteProjectModel
+    {
+        public string Error { get; set; }
+
+        public void DeleteProject(IMakerClient client, string id)
+        {
+            var project = client.ReadProject(id);
+            if (project != null && String.IsNullOrEmpty(project.Error))
+            {
+                if (!String.IsNullOrEmpty(project.Project.MasterLocation))
+                {
+                    DeleteMasterImage(project.Project.MasterLocation);
+                }
+                if (!String.IsNullOrEmpty(project.Project.MosaicLocation))
+                {
+                    DeleteMosaicImage(project.Project.MosaicLocation);
+                }
+            }
+            else
+            {
+                Error = "Error in fetching project with id";
+            }
+        }
+
+        public void DeleteMasterImage(string masterLocation)
+        {
+            if (File.Exists(masterLocation))
+            {
+                File.Delete(masterLocation);
+            }
+            else
+            {
+                Error = "Master Image does not exist";
+            }
+        }
+
+        public void DeleteMosaicImage(string mosaicLocation)
+        {
+            if (File.Exists(mosaicLocation))
+            {
+                File.Delete(mosaicLocation);
+            }
+            else
+            {
+                Error = "Mosaic Image does not exist";
+            }
+        }
+    }
+}
---
+++ Mosaic/Mosaic.WebUI/Mosaic.WebUITests/Models/DeleteProjectModelTests.cs
@@ -0,0 +1,78 @@
+﻿using Infrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Bson;
+using Moq;
+using Mosaic.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mosaic.WebUITests.Models
+{
+    [TestClass]
+    public class DeleteProjectModelTests
+    {
+        Mock<IMakerClient> MockMakerClient;
+
+        [TestInitialize]
+        public void SetUpRepository()
+        {
+            MockMakerClient = new Mock<IMakerClient>();
+        }
+
+        [TestMethod]
+        public void DeleteProjectReturnsErrorIfReadProjectReturnsError()
+        {
+            MockMakerClient.Setup(x => x.ReadProject(It.IsAny<string>())).Returns(new ProjectResponse { Error = "Error" });
+
+            var model = new DeleteProjectModel();
+            model.DeleteProject(MockMakerClient.Object, ObjectId.GenerateNewId().ToString());
+            Assert.IsFalse(String.IsNullOrEmpty(model.Error));
+        }
+
+        [TestMethod]
+        public void DeleteMasterImageReturnErrorIfImageDoesNotExist()
+        {
+            var project = new ProjectResponse(){ Project = new ProjectStructure()
+            {
+                Id = ObjectId.GenerateNewId().ToString(),
+                TimeOfCreation = DateTime.Now.ToString(),
+                Progress = ProjectStructure.Types.State.Largeadded,
+                MasterLocation = "InvalidLocation"
+            }};
+            MockMakerClient.Setup(x => x.ReadProject(It.IsAny<string>())).Returns(new ProjectResponse { Error = "Error" });
+
+            var model = new DeleteProjectModel();
+            model.DeleteProject(MockMakerClient.Object, ObjectId.GenerateNewId().ToString());
+            Assert.IsFalse(String.IsNullOrEmpty(model.Error));
+        }
+
+        [TestMethod]
+        public void DeleteMasterWillDeleteLocalMasterFileIfExists()
+        {
+            MockMakerClient.Setup(x => x.ReadProject(It.IsAny<string>())).Returns(new ProjectResponse { Error = "Error" });
+
+            var model = new DeleteProjectModel();
+            model.DeleteProject(MockMakerClient.Object, ObjectId.GenerateNewId().ToString());
+            Assert.IsFalse(String.IsNullOrEmpty(model.Error));
+        }
+
+        [TestMethod]
+        public void DeleteMosaicImageReturnErrorIfImageDoesNotExist()
+        {
+
+        }
+
+        [TestMethod]
+        public void DeleteMosaicWillDeleteLocalMosaicFileIfExists()
+        {
+
+        }
+
+        [TestMethod]
+        public void DeleteProjectWillReturnErrorIfProjectCannotBeDeleted()
+        {
+
+        }
+    }
+}
